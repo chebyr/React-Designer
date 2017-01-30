e063e4f645f0658e022d1e00bba870369776f55f
Konstantin S. <havendv@gmail.com>
2017-01-31 03:13:24 +1000

    Added project for JsToHtml translation with tests.

---
+++ JsHtmlConverter.Tests/JsHtmlConverter.Test.cs
@@ -0,0 +1,18 @@
+﻿namespace JsHtmlConverter.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class JsHtmlConverter_Test
+    {
+        [Test]
+        public void JsHtmlConverter_Simple()
+        {
+            var jsx = "";
+            var expected = "";
+            var actual = JsHtmlConverter.ConvertToHtml(jsx, "<h1>Hello, world!</h1>");
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ JsHtmlConverter/JsHtmlConverter.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using JavaScriptEngineSwitcher.Core;
+using JavaScriptEngineSwitcher.V8;
+using React;
+
+namespace JsHtmlConverter
+{
+    public static class JsHtmlConverter
+    {
+        public static string ConvertToHtml(string js)
+        {
+            var configuration = new ReactSiteConfiguration();
+            var filesystem = new SimpleFileSystem();
+            var cache = new MemoryFileCache();
+            var hash = new FileCacheHash();
+            JsEngineSwitcher.Instance.EngineFactories.AddV8();
+            using (var factory = new JavaScriptEngineFactory(JsEngineSwitcher.Instance, configuration, filesystem))
+            {
+                using (var environment = new ReactEnvironment(factory, configuration, cache, filesystem, hash))
+                {
+                    var babeled = environment.Babel.Transform(js);
+                    Debug.WriteLine($@"Babel output:
+{babeled}");
+
+                    return environment.Execute<string>(babeled);
+                }
+            }
+        }
+
+        public static string ConvertToHtml(string jsx, string name)
+        {
+            return ConvertToHtml($@"{jsx}
+
+ReactDOMServer.renderToString(
+  {name}
+);");
+        }
+    }
+}
